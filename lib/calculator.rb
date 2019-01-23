25ad691c1229fc4a611c2e8e6a5f660f1c9f8d27
rchuaml <ronchuaml@gmail.com>
2019-01-23 15:56:14 +0800

    fixed rubocop offenses

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -14,24 +14,23 @@ def divide(number1, number2)
   number1 / number2
 end
 
-puts "What calculation would you like to do? (add, sub, mult, div)"
-userInput = gets.chomp
-  puts "What is number 1?"
-  number1 = gets.chomp
-  number1 = Integer(number1)
-  puts "What is number 2?"
-  number2 = gets.chomp
-  number2 = Integer(number2)
+puts 'What calculation would you like to do? (add, sub, mult, div)'
+user_input = gets.chomp
+puts 'What is number 1?'
+number1 = gets.chomp
+number1 = Integer(number1)
+puts 'What is number 2?'
+number2 = gets.chomp
+number2 = Integer(number2)
 
-if userInput == "add"
-  answer = add(number1,number2)
-elsif userInput == "sub"
-  answer = subtract(number1,number2)
-elsif userInput == "mult"
-  answer = multiply(number1,number2)
-elsif userInput == "div"
-  answer = divide(number1,number2)
+if user_input == 'add'
+  answer = add(number1, number2)
+elsif user_input == 'sub'
+  answer = subtract(number1, number2)
+elsif user_input == 'mult'
+  answer = multiply(number1, number2)
+elsif user_input == 'div'
+  answer = divide(number1, number2)
 end
 
 puts "Your result is #{answer}"
-
