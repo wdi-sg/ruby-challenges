629d571ef9d106e8bd349dcdc6beb8fe8084e55b
ishakbhn <ishakbhn@gmail.com>
2019-01-23 17:04:40 +0800

    done subt use swtich

--- calculator.rb
+++ calculator.rb
@@ -10,11 +10,30 @@ def add()
   puts "Your Result is #{total}"
 end
 
-puts "What calculation you would like to do? (add, sub, mult, div)"
-answer = gets.chomp
+def subt()
+  puts "What is number 1?"
+  num1 = gets.chomp.to_i
 
-if answer == 'add'
-  puts add()
+  puts "What is number 2?"
+  num2 = gets.chomp.to_i
+
+  total = num1 - num2
+
+  puts "Your Result is #{total}"
 end
 
+puts "What calculation you would like to do? (add, subt, mult, div)"
+answer = gets.chomp
 
+# if answer == 'add'
+#   puts add()
+# end
+
+case answer
+when 'add'
+  puts add()
+when 'subt'
+  puts subt()
+else
+  "You can only add, sub, mult or div! Simply not interested!"
+end
