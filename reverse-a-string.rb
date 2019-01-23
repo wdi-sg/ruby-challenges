2ad93fc4834b7cb3146c22e162b978a92450c1ec
ishakbhn <ishakbhn@gmail.com>
2019-01-23 18:53:24 +0800

    done challenge 2 reverse

---
+++ reverse-a-string.rb
@@ -0,0 +1,13 @@
+puts "Enter a string"
+user_input = gets.chomp.split('')
+
+$reverse = []
+
+i = user_input.length - 1
+while i >= 0 do
+  $reverse.push(user_input[i])
+  i -= 1
+end
+
+puts "#{$reverse.join}"
+
