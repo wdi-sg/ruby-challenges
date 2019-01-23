130614a4fbb50f547280f8f4947ad7a9e0b5c57f
ishakbhn <ishakbhn@gmail.com>
2019-01-23 23:53:56 +0800

    done challenge 4

--- bank-transactions.rb
+++ bank-transactions.rb
@@ -1,7 +1,7 @@
 def deposit
- puts 'How much would you like to deposit?'
- transaction = gets.chomp.to_i
- $savings = $savings + transaction
+  puts 'How much would you like to deposit?'
+  transaction = gets.chomp.to_i
+  $savings = $savings + transaction
  puts check_balance
 end
 
@@ -9,7 +9,7 @@ def withdraw
   puts 'How much would you like to withdraw?'
   transaction = gets.chomp.to_i
   if transaction > $savings
-    puts 'The savings account is insufficent for #{transaction}'
+    puts "The savings account is insufficent for #{transaction}"
     puts check_balance
   elsif transaction <= $savings
     $savings = $savings - transaction
@@ -28,15 +28,14 @@ end
 def finish
   puts 'Are you done? (yes or no)'
   answer = gets.chomp
-    if answer == 'yes'
-      puts 'Thank You'
-    elsif answer == 'no'
-      puts welcome
-    else
-      puts 'Invalid option. yes or no only!'
-    end
+  if answer == 'yes'
+    puts 'Thank You'
+  elsif answer == 'no'
+    puts welcome
+  else
+    puts 'Invalid option. yes or no only!'
+  end
 end
-
 
 def welcome
   puts 'What would you like to do? (deposit, withdraw, check balance)'
---
+++ guessing-game.rb
@@ -0,0 +1,21 @@
+def check(user_input)
+  number = 19
+  $count = 0;
+
+  i = user_input
+  until i == number do
+    if i > number
+      puts "The number is lower than #{i}. Guess again"
+      $count += 1
+    elsif i < number
+      puts "The number is higher than #{i}. Guess again"
+      $count += 1
+    end
+    i = gets.chomp.to_i
+  end
+  puts "You got it in #{$count} tries"
+end
+
+puts "Guess a number between 1 and 100"
+puts check(gets.chomp.to_i)
+
