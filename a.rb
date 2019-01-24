6ce64fda6e348a779cc7d54b0a47dfc28f45fb0b
kauvia <mkhw1989@gmail.com>
2019-01-24 10:16:23 +0800

    added in find method

--- a.rb
+++ a.rb
@@ -6,6 +6,7 @@ class Node
     @children = []
     @end = false
   end
+
   def add(value)
     @children.each do |x|
       if x.value == value
@@ -16,6 +17,15 @@ class Node
     @children << child
     child
   end
+
+  def find(value)
+    @children.each do |x|
+      if x.value == value
+        return x
+      end
+    end
+    return nil
+  end
  end
 ########################## need to add retreive() and check() methods ###################
 
@@ -24,22 +34,37 @@ class Tree
   def initialize(root=nil)
     @root=Node.new(nil)
   end
+
   def add (word)
     node = @root
     word.length.times do |x|
       child = node.add(word[x])
-      node=child
+      node = child
     end
-    node.end =true
+    node.end = true
   end
- 
+
+  def find (word)
+    node = @root
+    word.length.times do |x|
+      child = node.find(word[x])
+      if child == nil then return p "#{word} not found"
+      else
+      node = child end
+    end
+    p "Found #{word}"
+  end
 end
 
 tree = Tree.new
 str = "apple"
 str2 = "appear"
 str3 = "banana"
+str4 = "ananas"
+
 tree.add(str)
 tree.add(str2)
 tree.add(str3)
- p tree
+
+tree.find("apple")
+tree.find(str4)
